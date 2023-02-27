00794e6a3fccb89d5b21d88b5d23ba1653681d21
小滴课堂大钊 <>
2023-02-27 23:37:18 +0800

    微信小程序项目我的页面开发《下》

--- src/composables/user.ts
+++ src/composables/user.ts
@@ -1,10 +1,30 @@
+import { getDetail } from '@/api/user';
+import type { IUser } from '@/typings/interface';
+
 export const useUser = defineStore(
   'user',
   () => {
-    let isLogin = $ref<boolean>(true);
+    let isLogin = $ref<boolean>(false);
+    let details = $ref<IUser>();
+    let token = $ref<string>('');
+
+    // 更改登录状态和获取用户数据
+    async function switchLoginState(_token: string) {
+      token = _token;
+      isLogin = true;
+      await syncUserInfo();
+      uni.switchTab({ url: '/pages/index/index' });
+    }
+
+    async function syncUserInfo() {
+      if (!token || !isLogin) return;
+      details = getDetail().data;
+    }
 
     return $$({
       isLogin,
+      switchLoginState,
+      details,
     });
   },
   {
--- src/pages/mine/components/LoginHeader.vue
+++ src/pages/mine/components/LoginHeader.vue
@@ -1,11 +1,15 @@
 <script lang="ts" setup>
-const { isLogin } = $(useUser())
+const { isLogin, details, switchLoginState } = $(useUser())
 
-const username = computed(() => (isLogin ? '大钊' : '点击登录'))
-const avatar = computed(() => (isLogin ? '/static/images/icons/mine/user.png' : '/static/images/icons/mine/user.png'))
+const username = computed(() => (isLogin ? details?.username : '点击登录'))
+const avatar = computed(() => (isLogin ? details?.headImg : '/static/images/icons/mine/user.png'))
 const learnTime = computed(() =>
-  isLogin ? `学习时长：${(100 / 3600).toFixed(2)}小时` : '新用户专属200D币礼包'
+  isLogin ? `学习时长：${(details?.learnTime as number / 3600).toFixed(2)}小时` : '新用户专属200D币礼包'
 )
+
+const toLogin = () => {
+  isLogin ? '' : switchLoginState('1')
+}
 
 
 </script>
@@ -16,7 +20,7 @@ const learnTime = computed(() =>
       <image :src="avatar" w-120 h-120 rounded-full class="b b-#f38e48" />
     </view>
     <view m="y-1 x-4" flex="~ col">
-      <text text="40 #4d555d">
+      <text text="40 #4d555d" @click="toLogin">
         {{ username }}
       </text>
       <view between w-full>
