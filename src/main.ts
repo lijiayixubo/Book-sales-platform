cf7bf5b08ad1c8e0de6b4513f4d8ad6d0e23fc70
小滴课堂大钊 <>
2023-02-27 12:04:35 +0800

    uni-app框架中使用Pinia状态管理的配置

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { createSSRApp } from 'vue';
 import App from './App.vue';
+import { createPinia } from 'pinia';
+import { createPersistedState } from 'pinia-plugin-persistedstate';
 import 'uno.css';
 
+// 引入Pinia和持久缓存的设置
+const pinia = createPinia().use(
+  createPersistedState({
+    storage: {
+      getItem(key: string) {
+        return uni.getStorageSync(key);
+      },
+      setItem(key: string, value: string) {
+        uni.setStorageSync(key, value);
+      },
+    },
+  })
+);
+
 export function createApp() {
-  const app = createSSRApp(App);
+  const app = createSSRApp(App).use(pinia);
   return {
     app,
   };
