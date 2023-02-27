1ad270ac9c870321e8f948822fd30dd281324c18
小滴课堂大钊 <>
2023-02-27 19:11:31 +0800

    微信小程序项目底部导航开发

---
+++ src/pages/category/index.vue
@@ -0,0 +1,11 @@
+<script lang='ts' setup>
+
+</script>
+
+<template>
+  <view>
+    分类
+  </view>
+</template>
+
+<style lang='less' scoped></style>
---
+++ src/pages/mine/index.vue
@@ -0,0 +1,11 @@
+<script lang='ts' setup>
+
+</script>
+
+<template>
+  <view>
+    我的
+  </view>
+</template>
+
+<style lang='less' scoped></style>
