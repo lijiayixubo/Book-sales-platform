1cf341bbccdb187a282ee3ff0e1f615397b00ee0
小滴课堂大钊 <>
2023-02-28 15:03:04 +0800

    1

--- src/pages/index/components/NavigationBar.vue
+++ src/pages/index/components/NavigationBar.vue
@@ -3,12 +3,8 @@ import Search from '@/components/Search.vue'
 </script>
 
 <template>
-  <view class="nav_height" />
-  <Search color="999999" background-color="#f6f6f6" />
+  <uni-nav-bar :fixed="true" :status-bar="true" :border="false" background-color="#f3f3f3" left-width="0" right-width="0">
+    <Search color="999999" background-color="#f6f6f6" />
+  </uni-nav-bar>
 </template>
 
-<style lang="scss" scoped>
-.nav_height {
-  height: var(--status-bar-height);
-}
-</style>
--- src/pages/study/components/NavigationBar.vue
+++ src/pages/study/components/NavigationBar.vue
@@ -3,24 +3,11 @@
 </script>
 
 <template>
-  <view class="nav_height" />
-  <view class="my_study">
-    我的学习
-  </view>
+  <uni-nav-bar :fixed="true" :status-bar="true" :border="false" background-color="#f3f3f3" left-width="0" right-width="0">
+    <view class="my_study">
+      我的学习
+    </view>
+  </uni-nav-bar>
 </template>
 
-<style lang="scss" scoped>
-.my_study {
-  color: #333333;
-  font-size: 36rpx;
-  font-weight: bold;
-  padding-left: 30rpx;
-  height: 44px;
-  background: #fff;
-  line-height: 44px;
-}
 
-.nav_height {
-  height: var(--status-bar-height);
-}
-</style>
